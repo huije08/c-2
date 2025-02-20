2f64037990e056f43bdceeab6d5387ca91f0ad4b
huije08 <huijegang@gmail.com>
2025-02-20 19:45:07 +0900

    (2025.2.20)

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -29,13 +29,11 @@ public:
 		if (head == nullptr)
 		{
 			head = newNode;
-
-			
 			newNode->next = head;
 		}
 		else
 		{
-			newNod->next = head->next;
+			newNode->next = head->next;
 
 			head->next = newNode;
 
@@ -44,21 +42,87 @@ public:
 		size++;
 	}
 
-	void show()
+	void push_front(T data)
 	{
+		Node* newNode = new Node;
+
+		if (head == nullptr)
+		{
+			head = newNode;
+			newNode->next = head;
+		}
+		else
+		{
+			newNode->next = head->next;
+
+			head->next = newNode;
+		}
+		size++;
+	}
+
+	void pop_front()
+	{
+		if (head == nullptr)
+		{
+			cout << "Linked List is Empty" << endl;
+		}
+		else
+		{
+			Node* deleteNode = head;
+
+			if (head == head->next)
+			{
+				head == nullptr;
+			}
+			else
+			{
+				head->next = deleteNode->next;
+			}
+			delete deleteNode;
+
+			size--;
+		}
 
 	}
 
+	void show()
+	{
+		if (head != nullptr)
+		{
+			Node* currentNode = head->next;
+
+			for (int i = 0; i < size; i++)
+			{
+				cout << currentNode->data << " ";
+				currentNode = currentNode->next;
+			}
+		}
+	}
+
+	
+
 	~CircleLinkedList()
 	{
+		while (head != nullptr)
+		{
+			pop_front();
+		}
 	}
 
 };
 
 int main()
 {
+	CircleLinkedList<int> circleLinkedList;
 
+	circleLinkedList.push_back(10);
+	circleLinkedList.push_back(20);
+	circleLinkedList.push_back(30);
+	
+	
+	circleLinkedList.pop_front();
 
+	circleLinkedList.show();
 
 	return 0;
 }
