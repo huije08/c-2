52ccf031c32d34c561cfc76da1b30f595289df76
huije08 <huijegang@gmail.com>
2025-02-28 17:11:22 +0900

    (2025.2.27) 이진트리

---
+++ Class7th (Vector)/Class7th (Vector).cpp
@@ -0,0 +1,110 @@
+﻿#include <iostream>
+using namespace std;
+
+template<typename T>
+class Vector
+{
+public:
+	Vector()
+	{
+		size = 0;
+		capacity = 0;
+		container = nullptr;
+
+	}
+
+	void resize(int newSize)
+	{
+		// 1. capacity에 새로운 size값을 저장합니다.
+		capacity = newSize;
+
+		// 2.새로운 포이널 변수를 생성해서 새롭게 만들어진
+		//	 메모리 공간을 가리키도록 합니다.
+		T* newContainer = new T[capacity];
+
+		// 3. 새로운 메모리의 공간의 값을 초기화합니다.
+		for (int i = 0; i < capacity; i++)
+		{
+			newContainer[i] = NULL;
+		}
+
+		// 4. 기존 배열에 있는 값을 복사해서 새로운 배열에
+		//    넣어줍니다.
+		for (int i = 0; i < size; i++)
+		{
+			newContainer[i] = container[i];
+		}
+
+		// 5. 기존 배열의 메모리 해제합니다.
+		if (container != nullptr)
+		{
+			delete[] container;
+		}
+
+		// 6. 기존에 배열을 가리키던 포인터 변수의 값을
+		//    새로운 배열의 시작주소로 가리킵니다.
+		container = newContainer;
+
+	}
+
+	void push_back(T data)
+	{
+		if (capacity <= 0)
+		{
+			resize(1);
+		}
+		else if (size >= capacity)
+		{
+			resize(capacity * 2);
+		}
+
+		container[size++] = data;
+	}
+
+	const int& Size()
+	{
+		return size;
+	}
+
+	const T& operator [] (const int& index)
+	{
+		return container[index];
+	}
+
+	void pop_back()
+	{
+		if (size <= 0)
+		{
+			cout << "Vector is empty" << endl;
+		}
+		else
+		{
+			container[--size] = NULL;
+		}
+	}
+
+private:
+	int size;
+	int capacity;
+
+	T* container;
+};
+
+int main()
+{
+	Vector<int> vector;
+
+	vector.push_back(10);
+	vector.push_back(20);
+	vector.push_back(30);
+	vector.push_back(40);
+	vector.push_back(50);
+
+	vector.pop_back();
+	for (int i = 0; i < vector.Size(); i++)
+	{
+		cout << vector[i + 1] << " ";
+	}
+
+	return 0;
+}
